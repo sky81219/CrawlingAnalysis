86aacd2d6d72ee50b1bebd80866933f9c0ec5eda
sky <sky81219>
2021-06-25 23:42:08 +0900

    셀레니움 Xpath 추가 검색 자동화 1차 완

--- Crawling/core_parsing/html_parser.py
+++ Crawling/core_parsing/html_parser.py
@@ -5,32 +5,37 @@ from core_parsing.parser_utility import FireFoxDriverUtility
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-
 # html data paring
-visit_deque = deque()
-
 class UrlParsingDriver:
     def __init__(self, data):
         self.data = data
         self.url_box = set()
         self.url = 'https://www.google.com'
-
-    def url_schema(self):
-        url_architecture = f'{urlparse(self.url).scheme}://{urlparse(self.url).netloc}'
-        link_string = re.compile("^(/|.*" + url_architecture + ")")
-
-        return url_architecture, link_string
+        self.visit_queue = deque()
 
     def html_parsing(self):
+        # 스키마 제작 function
+        def url_schema():
+            url_architecture = f'{urlparse(self.url).scheme}://{urlparse(self.url).netloc}'
+            link_string = re.compile("^(/|.*" + url_architecture + ")")
+
+            return url_architecture, link_string
+
         # 드라이버 가져오기 and html parsing
         web_driver = FireFoxDriverUtility()
         html = web_driver.search_injection(self.data)
 
+        # parsing
         for html_data in html:
-            # parsing
             soup = BeautifulSoup(html_data, 'lxml')
-            print(soup.prettify()+'\n')
-        """
-        for link in soup.find_all('h3', {'class': 'LC20lb DKV0Md'}):
-            print(f'{link}')
-        """
+            for href_tag in soup.findAll('a', href=url_schema()[1]):
+                href = href_tag.attrs['href']
+                if href is not None:
+                    if href.startswith('/'):
+                        print(url_schema()[0] + href)
+                    else:
+                        print(href)
+
+            for title_tag in soup.findAll('h3'):
+                title = title_tag.attrs['h3']
+                print(title)
--- Crawling/core_parsing/parser_utility.py
+++ Crawling/core_parsing/parser_utility.py
@@ -4,6 +4,8 @@
 crawling -> JSON - redis DB
 """
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from urllib.error import HTTPError, URLError
 import time
 
 
@@ -29,17 +31,27 @@ class FireFoxDriverUtility:
         self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
         # page search count
-        for i in range(2, count):
-            # 다음 페이지 가기위한 xpath 절차 딜레이 3초
-            next_page = self.driver.find_element_by_xpath(f'//a[@aria-label="Page {str(i)}"]')
-            next_page.click()
-            self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-            time.sleep(3)
+        try:
+            for i in range(2, count):
+                print(f'Search in Crawling... {i} page Checking')
+                # 다음 페이지 가기위한 xpath 절차 딜레이 3초
+                next_page = self.driver.find_element_by_xpath(f'//a[@aria-label="Page {str(i)}"]')
+                next_page.click()
+                self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+                time.sleep(3)
 
-            # html data -> html_parsing.py 로 return
-            html = self.driver.page_source
-            self.html_source.append(html)
-        self.driver.close()
+                # html data -> html_parsing.py 로 return
+                html = self.driver.page_source
+                self.html_source.append(html)
+            self.driver.close()
 
-        return self.html_source
+            return self.html_source
+        except KeyError:
+            raise KeyError
+        except HTTPError:
+            raise HTTPError
+        except NoSuchElementException:
+            print(NoSuchElementException)
+        finally:
+            print('Done..!')
 
